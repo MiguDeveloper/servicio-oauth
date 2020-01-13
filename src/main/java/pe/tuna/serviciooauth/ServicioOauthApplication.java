df28c8a5b37386e2a51ccbf1e80cd955de1e827d
MiguDeveloper <miguel.peru.seo@gmail.com>
2020-01-13 10:09:50 -0500

    setup project

---
+++ src/main/java/pe/tuna/serviciooauth/ServicioOauthApplication.java
@@ -0,0 +1,13 @@
+package pe.tuna.serviciooauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicioOauthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServicioOauthApplication.class, args);
+    }
+
+}
